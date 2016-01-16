584eda27c33ba356f08b8679e049d06dcf99e40c
megatran <nhantrantnt@gmail.com>
2016-01-16 03:23:24 -0700

    addd InAppBrowser plugin to open up links in default browser app

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -16,7 +16,7 @@ app.controller('FeedCtrl', function ($scope, $ionicLoading, FeedService) {
 	};
 });
 
-app.controller('PostCtrl', function ($scope, $stateParams, FeedService) {
+app.controller('PostCtrl', function ($scope, $stateParams, $window, FeedService) {
 	console.log("Loading PostCtrl");
 	console.log($stateParams);
 	$scope.postId = $stateParams.id;
@@ -28,6 +28,9 @@ app.controller('PostCtrl', function ($scope, $stateParams, FeedService) {
 
 	$scope.readMore = function () {
 		console.debug("Read more post");
+		$window.open($scope.post.link, "_system", "location=yes"); 
+		//_blank means open in new window on computer while _system open link in default browser on mobile devices.
+		//if running on real device, must install cordova in app broswer plugin to open in new browser
 	};
 
 });
